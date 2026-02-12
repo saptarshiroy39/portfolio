fcc7433c3994941ea67a04c1c5608c9316f66dac
Saptarshi Roy <saptarshiroy39@gmail.com>
2026-02-12 07:47:46 +0530

    Updated Intro Section

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -13,13 +13,13 @@ import { Java } from "@/components/ui/svgs/java";
 import { Csharp } from "@/components/ui/svgs/csharp";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Saptarshi Roy",
+  initials: "SR",
+  url: "https://hirishi.in",
+  location: "Kolkata, IN",
+  locationLink: "https://www.google.com/maps/place/Kolkata,+West+Bengal,+India",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer turned Entrepreneur. I love building things and helping people.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
